723f261f32b15547f74d923b4fac08f6c584f0fa
KatrinAleinik <katrinaleinik@gmail.com>
2019-05-24 23:50:54 -0500

    negative assertion

--- src/test/java/ObjectBingoSearch.java
+++ src/test/java/ObjectBingoSearch.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,13 +12,14 @@ public class ObjectBingoSearch {
     By searchField = By.xpath("//*[@id='sb_form_q']");
     By successMessageLocator = By.xpath("//*[@id='b_header']/nav/ul/li[1]/a");
     By firstElementInTheQueu = By.xpath("/html[1]/body[1]/div[2]/main[1]/ol[1]/li[1]/h2[1]/a[1]");
+    By notResultsFound = By.cssSelector(".b_no > h1:nth-child(1)");
     WebDriverWait wait;
     WebElement element;
 
     public  ObjectBingoSearch (WebDriver driver){
         this.driver = driver;
         driver.get("https://www.bing.com/");
-        wait = new WebDriverWait(driver,20);
+        wait = new WebDriverWait(driver,10);
         element  = wait.until(ExpectedConditions.visibilityOfElementLocated(searchField));
     }
 
@@ -32,13 +30,24 @@ public class ObjectBingoSearch {
     }
 
     public boolean GettingFirstRequest (String expected){
+        try{
         element  = wait.until(ExpectedConditions.visibilityOfElementLocated(firstElementInTheQueu));
         String ex = driver.findElement(firstElementInTheQueu).getText();
         ex = ex.toLowerCase();
         expected = expected.toLowerCase();
         boolean result = ex.contains(expected);
-        return result;
-    }
+        return result;}
+
+        catch (TimeoutException e){
+            element  = wait.until(ExpectedConditions.visibilityOfElementLocated(notResultsFound));
+            String ex = driver.findElement(notResultsFound).getText();
+            ex = ex.toLowerCase();
+            expected = expected.toLowerCase();
+            boolean result = ex.contains(expected);
+            return result;
+            }
+        }
+
 
 
     public boolean successSearch(){
