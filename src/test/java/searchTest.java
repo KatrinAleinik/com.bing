367f61c2d32b874370187b9b78e7a3325cde5de9
KatrinAleinik <katrinaleinik@gmail.com>
2019-06-06 00:42:40 -0500

    DataProvider

--- src/test/java/searchTest.java
+++ src/test/java/searchTest.java
@@ -4,65 +4,73 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.annotations.Test;
+import atu.testrecorder.ATUTestRecorder;
 
-import java.util.concurrent.TimeUnit;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 
 public class searchTest {
     private WebDriver driver;
     private  ObjectBingoSearch test;
     private boolean expected = true;
+    //ATUTestRecorder recorder;
+
+
+    @DataProvider (name = "validInput")
+    public Object [] [] createValidData(){
+        return new Object[][]{
+                {"Portnov computer school", "Portnov"},
+                {"Запрос на русском языке", "русском"},
+                {"1234567890", "1234567890"},
+        };
+    }
+
+    @DataProvider (name = "invalidInput")
+    public Object [][] createInvalidData(){
+        return new Object [][]{
+                {"!@#$%^&*()", "!@#$%^&*()"},
+        };
+    }
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         //System.setProperty("webdriver.gecko.driver", "/Users/greatkate/IdeaProjects/TestNG/src/test/recources/driver/geckodriver");
         //driver = new FirefoxDriver();
+
         System.setProperty("webdriver.chrome.driver","/Users/greatkate/IdeaProjects/comBingo/src/main/resources/chromedriver 2");
         driver = new ChromeDriver();
         test = new ObjectBingoSearch(driver);
     }
 
-    @Test
-    public void searchReq01(){
-        test.search("QA test engineers are wanted");
+    @Test (dataProvider = "validInput")
+    public void searchValidInput(String request, String result) throws Exception{
+        test.search(request);
         Assert.assertEquals(test.successSearch(),expected );
-        Assert.assertEquals(test.GettingFirstRequest("QA"),expected );
+        Assert.assertEquals(test.GettingFirstRequest(result),expected );
 
     }
 
-    @Test
-   public void searchReq02(){
-        test.search("1234567890");
+    @Test (dataProvider = "invalidInput")
+    public void searchInvalidInput(String request, String result) throws Exception{
+        test.search(request);
         Assert.assertEquals(expected, test.successSearch());
-        Assert.assertEquals(test.GettingFirstRequest("1234567890"),expected );
-
-    }
-
-
-    @Test
-    public void searchReq03(){
-        test.search("Запрос на русском языке");
-        Assert.assertEquals(expected, test.successSearch());
-        Assert.assertEquals(test.GettingFirstRequest("Запрос"),expected );
-
-    }
-
-    @Test
-    public void searchReq04(){
-        test.search("!@#$%^&*()");
-        Assert.assertEquals(expected, test.successSearch());
-        Assert.assertEquals(test.GettingFirstRequest("!@#$%^&*()"),expected );
+        Assert.assertEquals(test.GettingFirstRequest(result),expected );
     }
 
 
 
 
     @AfterMethod (alwaysRun =  true)
-    public void cookie(){
+    public void cookie() throws Exception{
 
         driver.manage().deleteAllCookies();
         driver.close();
+        //recorder.stop();
     }
+
 
 
 
